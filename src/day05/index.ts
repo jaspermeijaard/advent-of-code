d333fddbd6b789743d6b45f33a1db6538a28b9c2
Jasper Meijaard <jaspermeijaard@gmail.com>
2022-12-05 11:31:33 +0100

    Day 5

--- src/day05/index.ts
+++ src/day05/index.ts
@@ -3,15 +3,9 @@ import Arr from "fp-ts/lib/Array.js"
 import { flow } from "fp-ts/lib/function.js"
 import { Parse } from "../utils/index.js"
 
-type Move = {
-  count: number
-  from: number
-  to: number
-}
-
 const parseMoves = flow(
   Parse.splitLines,
-  Arr.map<string, Move>((raw: string) => {
+  Arr.map((raw: string) => {
     const [count, from, to] = raw.match(/(\d+)/g) as string[]
     return {
       count: Parse.toNumber(count),
@@ -33,31 +27,24 @@ const parseInput = flow(Parse.splitParagraphs, ([rawStacks, rawMoves]) => ({
   moves: parseMoves(rawMoves),
 }))
 
-const runMovesOnStacks =
-  (reverse: boolean = false) =>
-  ({ stacks, moves }: { stacks: string[][]; moves: Move[] }) => {
-    const result = [...stacks]
-    for (const { count, from, to } of moves) {
-      const containers = [...result[from].splice(-count)]
-      result[to].push(...(reverse ? containers.reverse() : containers))
-    }
+const runMoves = (reverse: boolean) =>
+  flow(
+    parseInput,
+    ({ stacks, moves }) => {
+      const result = [...stacks]
+      for (const { count, from, to } of moves) {
+        const containers = [...result[from].splice(-count)]
+        result[to].push(...(reverse ? containers.reverse() : containers))
+      }
 
-    return result
-  }
+      return result
+    },
+    Arr.map((s) => s.pop()),
+    (s) => s.join(""),
+  )
 
-const part1 = flow(
-  parseInput,
-  runMovesOnStacks(),
-  Arr.map((s) => s.pop()),
-  (s) => s.join(""),
-)
-
-const part2 = flow(
-  parseInput,
-  runMovesOnStacks(true),
-  Arr.map((s) => s.pop()),
-  (s) => s.join(""),
-)
+const part1 = runMoves(false)
+const part2 = runMoves(true)
 
 run({
   part1: { solution: part1 },
