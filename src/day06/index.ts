ade12953c19970a1a7bc67e33ab0928e992dba85
Jasper Meijaard <jaspermeijaard@gmail.com>
2022-12-06 08:52:14 +0100

    Day 6

--- src/day06/index.ts
+++ src/day06/index.ts
@@ -2,7 +2,7 @@ import run from "aocrunner"
 import { flow } from "fp-ts/lib/function.js"
 import { Parse } from "../utils/index.js"
 
-const findLetterSequence = (size: number) => (arr: string[]) =>
+const findLetterSequences = (size: number) => (arr: string[]) =>
   arr.reduce<number[]>((matches, _, i, all) => {
     const slice = all.slice(Math.max(0, i), Math.min(i + size, all.length))
     return slice.length === size && new Set(slice).size === slice.length
@@ -10,8 +10,8 @@ const findLetterSequence = (size: number) => (arr: string[]) =>
       : matches
   }, [])
 
-const part1 = flow(Parse.splitChars, findLetterSequence(4), (a) => a.shift())
-const part2 = flow(Parse.splitChars, findLetterSequence(14), (a) => a.shift())
+const part1 = flow(Parse.splitChars, findLetterSequences(4), (a) => a.shift())
+const part2 = flow(Parse.splitChars, findLetterSequences(14), (a) => a.shift())
 
 run({
   part1: { solution: part1 },
